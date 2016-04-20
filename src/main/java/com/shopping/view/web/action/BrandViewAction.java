fc2660a66839e476a8d2ae55a5160c8f08eb15b3
huanghuanlai <102535481@qq.com>
2016-04-20 17:41:16 +0800

    优化配置文件

--- src/main/java/com/shopping/view/web/action/BrandViewAction.java
+++ src/main/java/com/shopping/view/web/action/BrandViewAction.java
@@ -1,6 +1,6 @@
- package com.shopping.view.web.action;
- 
- import java.io.IOException;
+package com.shopping.view.web.action;
+
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -31,129 +31,140 @@ import com.shopping.foundation.service.IGoodsService;
 import com.shopping.foundation.service.ISysConfigService;
 import com.shopping.foundation.service.IUserConfigService;
 import com.shopping.view.web.tools.StoreViewTools;
- 
- @Controller
- public class BrandViewAction
- {
- 
-   @Autowired
-   private ISysConfigService configService;
- 
-   @Autowired
-   private IUserConfigService userConfigService;
- 
-   @Autowired
-   private IGoodsClassService goodsClassService;
- 
-   @Autowired
-   private IGoodsService goodsService;
- 
-   @Autowired
-   private IGoodsBrandService goodsBrandService;
- 
-   @Autowired
-   private IGoodsBrandCategoryService goodsBrandCategorySerivce;
- 
-   @Autowired
-   private StoreViewTools storeViewTools;
- 
-   @RequestMapping({"/brand.htm"})
-   public ModelAndView brand(HttpServletRequest request, HttpServletResponse response, String gbc_id)
-   {
-     ModelAndView mv = new JModelAndView("brand.html", this.configService.getSysConfig(), 
-       this.userConfigService.getUserConfig(), 1, request, response);
-     
-     String shopping_view_type = CommUtil.null2String( request.getSession( false ).getAttribute( "shopping_view_type" ) );
-     
-	 if( (shopping_view_type != null) && (!shopping_view_type.equals( "" )) && (shopping_view_type.equals( "wap" )) ) {
-		 mv = new JModelAndView("wap/brand.html", this.configService.getSysConfig(), 
-			       this.userConfigService.getUserConfig(), 1, request, response);
-	 }
-     List gbcs = this.goodsBrandCategorySerivce.query("select obj from GoodsBrandCategory obj  order by obj.addTime asc", null, -1, -1);
-     Map params = new HashMap();
-     params.put("recommend", Boolean.valueOf(true));
-     params.put("audit", Integer.valueOf(1));
-     List gbs = this.goodsBrandService.query("select obj from GoodsBrand obj where obj.recommend=:recommend and obj.audit=:audit order by obj.sequence asc", params, 0, 10);
-     mv.addObject("gbs", gbs);
-     mv.addObject("gbcs", gbcs);
-     List<GoodsBrand> brands = new ArrayList();
-     if ((gbc_id != null) && (!gbc_id.equals(""))) {
-       mv.addObject("gbc_id", gbc_id);
-       params.clear();
-       params.put("gbc_id", CommUtil.null2Long(gbc_id));
-       params.put("audit", Integer.valueOf(1));
-       brands = this.goodsBrandService.query("select obj from GoodsBrand obj where obj.category.id=:gbc_id and obj.audit=:audit order by obj.sequence asc", params, -1, -1);
-     } else {
-       params.clear();
-       params.put("audit", Integer.valueOf(1));
-       brands = this.goodsBrandService.query("select obj from GoodsBrand obj where obj.audit=:audit order by obj.sequence asc", params, -1, -1);
-     }
-     List all_list = new ArrayList();
-     String list_word = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
-     String[] words = list_word.split(",");
-     for (String word : words) {
-       Map brand_map = new HashMap();
-       List brand_list = new ArrayList();
-       for (GoodsBrand gb : brands) {
-         if ((CommUtil.null2String(gb.getFirst_word()).equals("")) || 
-           (!word.equals(gb.getFirst_word().toUpperCase()))) continue;
-         brand_list.add(gb);
-       }
- 
-       brand_map.put("brand_list", brand_list);
-       brand_map.put("word", word);
-       all_list.add(brand_map);
-     }
-     mv.addObject("all_list", all_list);
-     return mv;
-   }
- 
-   @RequestMapping({"/brand_goods.htm"})
-   public ModelAndView brand_view(HttpServletRequest request, HttpServletResponse response, String id, String currentPage, String orderBy, String orderType, String store_price_begin, String store_price_end, String op, String goods_name)
-   {
-     ModelAndView mv = new JModelAndView("brand_goods.html", this.configService.getSysConfig(), 
-       this.userConfigService.getUserConfig(), 1, request, response);
-     
-     String shopping_view_type = CommUtil.null2String( request.getSession( false ).getAttribute( "shopping_view_type" ) );
-     
-	 if( (shopping_view_type != null) && (!shopping_view_type.equals( "" )) && (shopping_view_type.equals( "wap" )) ) {
-		 mv = new JModelAndView("wap/brand_goods.html", this.configService.getSysConfig(), 
-			       this.userConfigService.getUserConfig(), 1, request, response);
-	 }
-     if ((op != null) && (!op.equals(""))) {
-       mv.addObject("op", op);
-     }
-     GoodsBrand gb = this.goodsBrandService.getObjById(CommUtil.null2Long(id));
-     mv.addObject("gb", gb);
-     Map params = new HashMap();
-     params.put("recommend", Boolean.valueOf(true));
-     params.put("audit", Integer.valueOf(1));
-     List gbs = this.goodsBrandService.query("select obj from GoodsBrand obj where obj.recommend=:recommend and obj.audit=:audit order by obj.sequence asc", params, 0, 10);
-     mv.addObject("gbs", gbs);
-     mv.addObject("storeViewTools", this.storeViewTools);
-     GoodsQueryObject gqo = new GoodsQueryObject(currentPage, mv, orderBy, orderType);
-     if ((store_price_begin != null) && (!store_price_begin.equals(""))) {
-       gqo.addQuery("obj.store_price", new SysMap("store_price_begin", BigDecimal.valueOf(CommUtil.null2Float(store_price_begin))), ">=");
-       mv.addObject("store_price_begin", store_price_begin);
-     }
-     if ((store_price_end != null) && (!store_price_end.equals(""))) {
-       gqo.addQuery("obj.store_price", new SysMap("store_price_end", BigDecimal.valueOf(CommUtil.null2Float(store_price_end))), "<=");
-       mv.addObject("store_price_end", store_price_end);
-     }
-     if ((goods_name != null) && (!goods_name.equals(""))) {
-       gqo.addQuery("obj.goods_name", new SysMap("goods_name", "%" + goods_name.trim() + "%"), "like");
-       mv.addObject("goods_name", goods_name);
-     }
-     gqo.addQuery("obj.goods_brand.id", new SysMap("goods_brand_id", gb.getId()), "=");
-     gqo.addQuery("obj.goods_status", new SysMap("goods_status", Integer.valueOf(0)), "=");
-     gqo.setPageSize(Integer.valueOf(20));
-     IPageList pList = this.goodsService.list(gqo);
-     CommUtil.saveIPageList2ModelAndView("", "", "", pList, mv);
-     return mv;
-   }
-   
-    /**
-     * wap根据品牌查询商品
+
+@Controller
+public class BrandViewAction {
+
+	@Autowired
+	private ISysConfigService configService;
+
+	@Autowired
+	private IUserConfigService userConfigService;
+
+	@Autowired
+	private IGoodsClassService goodsClassService;
+
+	@Autowired
+	private IGoodsService goodsService;
+
+	@Autowired
+	private IGoodsBrandService goodsBrandService;
+
+	@Autowired
+	private IGoodsBrandCategoryService goodsBrandCategorySerivce;
+
+	@Autowired
+	private StoreViewTools storeViewTools;
+
+	@RequestMapping({ "/brand.htm" })
+	public ModelAndView brand(HttpServletRequest request, HttpServletResponse response, String gbc_id) {
+		ModelAndView mv = new JModelAndView("brand.html", this.configService.getSysConfig(),
+				this.userConfigService.getUserConfig(), 1, request, response);
+
+		String shopping_view_type = CommUtil.null2String(request.getSession(false).getAttribute("shopping_view_type"));
+
+		if ((shopping_view_type != null) && (!shopping_view_type.equals("")) && (shopping_view_type.equals("wap"))) {
+			mv = new JModelAndView("wap/brand.html", this.configService.getSysConfig(),
+					this.userConfigService.getUserConfig(), 1, request, response);
+		}
+		List gbcs = this.goodsBrandCategorySerivce
+				.query("select obj from GoodsBrandCategory obj  order by obj.addTime asc", null, -1, -1);
+		Map params = new HashMap();
+		params.put("recommend", Boolean.valueOf(true));
+		params.put("audit", Integer.valueOf(1));
+		List gbs = this.goodsBrandService.query(
+				"select obj from GoodsBrand obj where obj.recommend=:recommend and obj.audit=:audit order by obj.sequence asc",
+				params, 0, 10);
+		mv.addObject("gbs", gbs);
+		mv.addObject("gbcs", gbcs);
+		List<GoodsBrand> brands = new ArrayList();
+		if ((gbc_id != null) && (!gbc_id.equals(""))) {
+			mv.addObject("gbc_id", gbc_id);
+			params.clear();
+			params.put("gbc_id", CommUtil.null2Long(gbc_id));
+			params.put("audit", Integer.valueOf(1));
+			brands = this.goodsBrandService.query(
+					"select obj from GoodsBrand obj where obj.category.id=:gbc_id and obj.audit=:audit order by obj.sequence asc",
+					params, -1, -1);
+		} else {
+			params.clear();
+			params.put("audit", Integer.valueOf(1));
+			brands = this.goodsBrandService.query(
+					"select obj from GoodsBrand obj where obj.audit=:audit order by obj.sequence asc", params, -1, -1);
+		}
+		List all_list = new ArrayList();
+		String list_word = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+		String[] words = list_word.split(",");
+		for (String word : words) {
+			Map brand_map = new HashMap();
+			List brand_list = new ArrayList();
+			for (GoodsBrand gb : brands) {
+				if ((CommUtil.null2String(gb.getFirst_word()).equals(""))
+						|| (!word.equals(gb.getFirst_word().toUpperCase())))
+					continue;
+				brand_list.add(gb);
+			}
+
+			brand_map.put("brand_list", brand_list);
+			brand_map.put("word", word);
+			all_list.add(brand_map);
+		}
+		mv.addObject("all_list", all_list);
+		return mv;
+	}
+
+	@RequestMapping({ "/brand_goods.htm" })
+	public ModelAndView brand_view(HttpServletRequest request, HttpServletResponse response, String id,
+			String currentPage, String orderBy, String orderType, String store_price_begin, String store_price_end,
+			String op, String goods_name) {
+		ModelAndView mv = new JModelAndView("brand_goods.html", this.configService.getSysConfig(),
+				this.userConfigService.getUserConfig(), 1, request, response);
+
+		String shopping_view_type = CommUtil.null2String(request.getSession(false).getAttribute("shopping_view_type"));
+
+		if ((shopping_view_type != null) && (!shopping_view_type.equals("")) && (shopping_view_type.equals("wap"))) {
+			mv = new JModelAndView("wap/brand_goods.html", this.configService.getSysConfig(),
+					this.userConfigService.getUserConfig(), 1, request, response);
+		}
+		if ((op != null) && (!op.equals(""))) {
+			mv.addObject("op", op);
+		}
+		GoodsBrand gb = this.goodsBrandService.getObjById(CommUtil.null2Long(id));
+		mv.addObject("gb", gb);
+		Map params = new HashMap();
+		params.put("recommend", Boolean.valueOf(true));
+		params.put("audit", Integer.valueOf(1));
+		List gbs = this.goodsBrandService.query(
+				"select obj from GoodsBrand obj where obj.recommend=:recommend and obj.audit=:audit order by obj.sequence asc",
+				params, 0, 10);
+		mv.addObject("gbs", gbs);
+		mv.addObject("storeViewTools", this.storeViewTools);
+		GoodsQueryObject gqo = new GoodsQueryObject(currentPage, mv, orderBy, orderType);
+		if ((store_price_begin != null) && (!store_price_begin.equals(""))) {
+			gqo.addQuery("obj.store_price",
+					new SysMap("store_price_begin", BigDecimal.valueOf(CommUtil.null2Float(store_price_begin))), ">=");
+			mv.addObject("store_price_begin", store_price_begin);
+		}
+		if ((store_price_end != null) && (!store_price_end.equals(""))) {
+			gqo.addQuery("obj.store_price",
+					new SysMap("store_price_end", BigDecimal.valueOf(CommUtil.null2Float(store_price_end))), "<=");
+			mv.addObject("store_price_end", store_price_end);
+		}
+		if ((goods_name != null) && (!goods_name.equals(""))) {
+			gqo.addQuery("obj.goods_name", new SysMap("goods_name", "%" + goods_name.trim() + "%"), "like");
+			mv.addObject("goods_name", goods_name);
+		}
+		gqo.addQuery("obj.goods_brand.id", new SysMap("goods_brand_id", gb.getId()), "=");
+		gqo.addQuery("obj.goods_status", new SysMap("goods_status", Integer.valueOf(0)), "=");
+		gqo.setPageSize(Integer.valueOf(20));
+		IPageList pList = this.goodsService.list(gqo);
+		CommUtil.saveIPageList2ModelAndView("", "", "", pList, mv);
+		return mv;
+	}
+
+	/**
+	 * wap根据品牌查询商品
+	 * 
 	 * @param request
 	 * @param response
 	 * @param id
@@ -166,42 +177,45 @@ import com.shopping.view.web.tools.StoreViewTools;
 	 * @param goods_name
 	 * @return
 	 */
-	@RequestMapping({"/brand_goods_ajax.htm"})
-   public void brand_view_ajax(HttpServletRequest request, HttpServletResponse response, String id, String currentPage, String orderBy, String orderType, String store_price_begin, String store_price_end, String op, String goods_name)
-   {
-	 Map<String, Object> map = new HashMap<String, Object>();
-	 if ((op != null) && (!op.equals(""))) {
-		 map.put("op", op);
-	 }
-     GoodsBrand gb = this.goodsBrandService.getObjById(CommUtil.null2Long(id));
-     
-     GoodsQueryObject gqo = new GoodsQueryObject(currentPage, map, orderBy, orderType);
-     
-     if ((store_price_begin != null) && (!store_price_begin.equals(""))) {
-       gqo.addQuery("obj.store_price", new SysMap("store_price_begin", BigDecimal.valueOf(CommUtil.null2Float(store_price_begin))), ">=");
-       map.put("store_price_begin", store_price_begin);
-     }
-     
-     if ((store_price_end != null) && (!store_price_end.equals(""))) {
-       gqo.addQuery("obj.store_price", new SysMap("store_price_end", BigDecimal.valueOf(CommUtil.null2Float(store_price_end))), "<=");
-       map.put("store_price_end", store_price_end);
-     }
-     
-     if ((goods_name != null) && (!goods_name.equals(""))) {
-       gqo.addQuery("obj.goods_name", new SysMap("goods_name", "%" + goods_name.trim() + "%"), "like");
-       map.put("goods_name", goods_name);
-     }
-     
-     gqo.addQuery("obj.goods_brand.id", new SysMap("goods_brand_id", gb.getId()), "=");
-     gqo.addQuery("obj.goods_status", new SysMap("goods_status", Integer.valueOf(0)), "=");
-     gqo.setPageSize(Integer.valueOf(12));
-     IPageList pList = this.goodsService.list(gqo);
-     
-     CommUtil.saveWebPaths(map, this.configService.getSysConfig(), request);
-     map.put("show", "brand_goods");
-     CommUtil.saveIPageList2Map("", "", "", pList, map);
-     
-	 String ret = Json.toJson(map, JsonFormat.compact());
+	@RequestMapping({ "/brand_goods_ajax.htm" })
+	public void brand_view_ajax(HttpServletRequest request, HttpServletResponse response, String id, String currentPage,
+			String orderBy, String orderType, String store_price_begin, String store_price_end, String op,
+			String goods_name) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		if ((op != null) && (!op.equals(""))) {
+			map.put("op", op);
+		}
+		GoodsBrand gb = this.goodsBrandService.getObjById(CommUtil.null2Long(id));
+
+		GoodsQueryObject gqo = new GoodsQueryObject(currentPage, map, orderBy, orderType);
+
+		if ((store_price_begin != null) && (!store_price_begin.equals(""))) {
+			gqo.addQuery("obj.store_price",
+					new SysMap("store_price_begin", BigDecimal.valueOf(CommUtil.null2Float(store_price_begin))), ">=");
+			map.put("store_price_begin", store_price_begin);
+		}
+
+		if ((store_price_end != null) && (!store_price_end.equals(""))) {
+			gqo.addQuery("obj.store_price",
+					new SysMap("store_price_end", BigDecimal.valueOf(CommUtil.null2Float(store_price_end))), "<=");
+			map.put("store_price_end", store_price_end);
+		}
+
+		if ((goods_name != null) && (!goods_name.equals(""))) {
+			gqo.addQuery("obj.goods_name", new SysMap("goods_name", "%" + goods_name.trim() + "%"), "like");
+			map.put("goods_name", goods_name);
+		}
+
+		gqo.addQuery("obj.goods_brand.id", new SysMap("goods_brand_id", gb.getId()), "=");
+		gqo.addQuery("obj.goods_status", new SysMap("goods_status", Integer.valueOf(0)), "=");
+		gqo.setPageSize(Integer.valueOf(12));
+		IPageList pList = this.goodsService.list(gqo);
+
+		CommUtil.saveWebPaths(map, this.configService.getSysConfig(), request);
+		map.put("show", "brand_goods");
+		CommUtil.saveIPageList2Map("", "", "", pList, map);
+
+		String ret = Json.toJson(map, JsonFormat.compact());
 		response.setContentType("text/plain");
 		response.setHeader("Cache-Control", "no-cache");
 		response.setCharacterEncoding("UTF-8");
@@ -211,12 +225,6 @@ import com.shopping.view.web.tools.StoreViewTools;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-   }
- 
- 
- }
+	}
 
-
- 
- 
- 
+}
--- src/main/java/com/shopping/view/web/action/InstallViewAction.java
+++ src/main/java/com/shopping/view/web/action/InstallViewAction.java
@@ -57,148 +57,150 @@ public class InstallViewAction implements ServletContextAware {
 	@Autowired
 	private DatabaseTools databaseTools;
 
-	@RequestMapping( { "/install.htm" } )
-	public ModelAndView install( HttpServletRequest request, HttpServletResponse response, String install_status, String title, String pws, String test_data ) {
-		ModelAndView mv = new JModelAndView( "WEB-INF/templates/install/install1.html", this.configService.getSysConfig(), this.userConfigService.getUserConfig(), 2, request, response );
-		if( CommUtil.null2String( install_status ).equals( "" ) ) {
-			mv.addObject( "install_status", "install1" );
+	@RequestMapping({ "/install.htm" })
+	public ModelAndView install(HttpServletRequest request, HttpServletResponse response, String install_status,
+			String title, String pws, String test_data) {
+		ModelAndView mv = new JModelAndView("WEB-INF/templates/install/install1.html",
+				this.configService.getSysConfig(), this.userConfigService.getUserConfig(), 2, request, response);
+		if (CommUtil.null2String(install_status).equals("")) {
+			mv.addObject("install_status", "install1");
 		}
-		if( CommUtil.null2String( install_status ).equals( "install1" ) ) {
-			mv = new JModelAndView( "WEB-INF/templates/install/install2.html", this.configService.getSysConfig(), this.userConfigService.getUserConfig(), 2, request, response );
-			mv.addObject( "install_status", "install2" );
+		if (CommUtil.null2String(install_status).equals("install1")) {
+			mv = new JModelAndView("WEB-INF/templates/install/install2.html", this.configService.getSysConfig(),
+					this.userConfigService.getUserConfig(), 2, request, response);
+			mv.addObject("install_status", "install2");
 		}
-		if( CommUtil.null2String( install_status ).equals( "install2" ) ) {
-			mv = new JModelAndView( "WEB-INF/templates/install/install3.html", this.configService.getSysConfig(), this.userConfigService.getUserConfig(), 2, request, response );
-			mv.addObject( "install_status", "install3" );
-			mv.addObject( "test_data", CommUtil.null2String( test_data ) );
-			String shop_url = CommUtil.getURL( request );
-			mv.addObject( "shop_url", shop_url );
-			mv.addObject( "title", CommUtil.null2String( title ) );
-			mv.addObject( "pws", CommUtil.null2String( pws ) );
-			String shop_manage_url = CommUtil.getURL( request ) + "/admin/index.htm";
-			mv.addObject( "shop_manage_url", shop_manage_url );
+		if (CommUtil.null2String(install_status).equals("install2")) {
+			mv = new JModelAndView("WEB-INF/templates/install/install3.html", this.configService.getSysConfig(),
+					this.userConfigService.getUserConfig(), 2, request, response);
+			mv.addObject("install_status", "install3");
+			mv.addObject("test_data", CommUtil.null2String(test_data));
+			String shop_url = CommUtil.getURL(request);
+			mv.addObject("shop_url", shop_url);
+			mv.addObject("title", CommUtil.null2String(title));
+			mv.addObject("pws", CommUtil.null2String(pws));
+			String shop_manage_url = CommUtil.getURL(request) + "/admin/index.htm";
+			mv.addObject("shop_manage_url", shop_manage_url);
 		}
-		mv.addObject( "version", "V2.0" );
+		mv.addObject("version", "V2.0");
 		return mv;
 	}
 
-	@RequestMapping( { "/install_over.htm" } )
-	public ModelAndView install_over( HttpServletRequest request, HttpServletResponse response ) {
-		ModelAndView mv = new JModelAndView( "success.html", this.configService.getSysConfig(), this.userConfigService.getUserConfig(), 1, request, response );
-		mv.addObject( "op_title", "您已经安装shopping商城系统，重新安装请删除install.lock文件" );
+	@RequestMapping({ "/install_over.htm" })
+	public ModelAndView install_over(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new JModelAndView("success.html", this.configService.getSysConfig(),
+				this.userConfigService.getUserConfig(), 1, request, response);
+		mv.addObject("op_title", "您已经安装shopping商城系统，重新安装请删除install.lock文件");
 		return mv;
 	}
 
-	@RequestMapping( { "/install_view.htm" } )
-	public ModelAndView install_view( HttpServletRequest request, HttpServletResponse response ) {
-		ModelAndView mv = new JModelAndView( "WEB-INF/templates/install/install_view.html", this.configService.getSysConfig(), this.userConfigService.getUserConfig(), 2, request, response );
+	@RequestMapping({ "/install_view.htm" })
+	public ModelAndView install_view(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new JModelAndView("WEB-INF/templates/install/install_view.html",
+				this.configService.getSysConfig(), this.userConfigService.getUserConfig(), 2, request, response);
 		return mv;
 	}
 
-	@RequestMapping( { "/install_init_test.htm" } )
-	public void install_init_test( HttpServletRequest request, HttpServletResponse response, String title, String pws ) throws Exception {
-		String path = request.getSession().getServletContext().getRealPath( "/" ) + "install.lock";
-		File file = new File( path );
-		if( !file.exists() ) {
-			String filePath = request.getSession().getServletContext().getRealPath( "/" ) + "resources/data/test.sql";
-			File sql_file = new File( filePath );
+	@RequestMapping({ "/install_init_test.htm" })
+	public void install_init_test(HttpServletRequest request, HttpServletResponse response, String title, String pws)
+			throws Exception {
+		String path = request.getSession().getServletContext().getRealPath("/") + "install.lock";
+		File file = new File(path);
+		if (!file.exists()) {
+			String filePath = request.getSession().getServletContext().getRealPath("/") + "resources/data/test.sql";
+			File sql_file = new File(filePath);
 			boolean ret = true;
-			if( sql_file.exists() ) {
-				this.databaseTools.executSqlScript( filePath );
+			if (sql_file.exists()) {
+				this.databaseTools.executSqlScript(filePath);
 			}
 			CacheManager manager = CacheManager.create();
 			manager.clearAll();
 			SysConfig config = this.configService.getSysConfig();
-			config.setAddTime( new Date() );
-			config.setTitle( title );
-			config.setWebsiteState( true );
-			this.configService.update( config );
-			User admin = this.userService.getObjByProperty( "userName", "admin" );
-			admin.setPassword( Md5Encrypt.md5( pws ).toLowerCase() );
-			this.userService.update( admin );
+			config.setAddTime(new Date());
+			config.setTitle(title);
+			config.setWebsiteState(true);
+			this.configService.update(config);
+			User admin = this.userService.getObjByProperty("userName", "admin");
+			admin.setPassword(Md5Encrypt.md5(pws).toLowerCase());
+			this.userService.update(admin);
 			manager = CacheManager.create();
 			manager.clearAll();
 
 			Map urlAuthorities = this.securityManager.loadUrlAuthorities();
-			this.servletContext.setAttribute( "urlAuthorities", urlAuthorities );
-			response.setContentType( "text/plain" );
-			response.setHeader( "Cache-Control", "no-cache" );
-			response.setCharacterEncoding( "UTF-8" );
+			this.servletContext.setAttribute("urlAuthorities", urlAuthorities);
+			response.setContentType("text/plain");
+			response.setHeader("Cache-Control", "no-cache");
+			response.setCharacterEncoding("UTF-8");
 			try {
 				file.createNewFile();
 				PrintWriter writer = response.getWriter();
-				writer.print( true );
-			}
-			catch( IOException e ) {
+				writer.print(true);
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		}
-		else {
-			response.setContentType( "text/plain" );
-			response.setHeader( "Cache-Control", "no-cache" );
-			response.setCharacterEncoding( "UTF-8" );
+		} else {
+			response.setContentType("text/plain");
+			response.setHeader("Cache-Control", "no-cache");
+			response.setCharacterEncoding("UTF-8");
 			try {
 				PrintWriter writer = response.getWriter();
-				writer.print( false );
-			}
-			catch( IOException e ) {
+				writer.print(false);
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	@RequestMapping( { "/install_init_base.htm" } )
-	public void install_init_base( HttpServletRequest request, HttpServletResponse response, String title, String pws ) throws Exception {
-		String path = request.getSession().getServletContext().getRealPath( "/" ) + "install.lock";
-		File file = new File( path );
-		if( !file.exists() ) {
-			String filePath = request.getSession().getServletContext().getRealPath( "/" ) + "resources/data/base.sql";
-			File sql_file = new File( filePath );
-			if( sql_file.exists() ) {
-				this.databaseTools.executSqlScript( filePath );
+	@RequestMapping({ "/install_init_base.htm" })
+	public void install_init_base(HttpServletRequest request, HttpServletResponse response, String title, String pws)
+			throws Exception {
+		String path = request.getSession().getServletContext().getRealPath("/") + "install.lock";
+		File file = new File(path);
+		if (!file.exists()) {
+			String filePath = request.getSession().getServletContext().getRealPath("/") + "resources/data/base.sql";
+			File sql_file = new File(filePath);
+			if (sql_file.exists()) {
+				this.databaseTools.executSqlScript(filePath);
 			}
 			CacheManager manager = CacheManager.create();
 			manager.clearAll();
 			SysConfig config = this.configService.getSysConfig();
-			config.setAddTime( new Date() );
-			config.setTitle( title );
-			config.setWebsiteState( true );
-			this.configService.update( config );
-			User admin = this.userService.getObjByProperty( "userName", "admin" );
-			admin.setPassword( Md5Encrypt.md5( pws ).toLowerCase() );
-			this.userService.update( admin );
+			config.setAddTime(new Date());
+			config.setTitle(title);
+			config.setWebsiteState(true);
+			this.configService.update(config);
+			User admin = this.userService.getObjByProperty("userName", "admin");
+			admin.setPassword(Md5Encrypt.md5(pws).toLowerCase());
+			this.userService.update(admin);
 			manager = CacheManager.create();
 			manager.clearAll();
 
 			Map urlAuthorities = this.securityManager.loadUrlAuthorities();
-			this.servletContext.setAttribute( "urlAuthorities", urlAuthorities );
-			response.setContentType( "text/plain" );
-			response.setHeader( "Cache-Control", "no-cache" );
-			response.setCharacterEncoding( "UTF-8" );
+			this.servletContext.setAttribute("urlAuthorities", urlAuthorities);
+			response.setContentType("text/plain");
+			response.setHeader("Cache-Control", "no-cache");
+			response.setCharacterEncoding("UTF-8");
 			try {
 				PrintWriter writer = response.getWriter();
 				file.createNewFile();
-				writer.print( true );
-			}
-			catch( IOException e ) {
+				writer.print(true);
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		}
-		else {
-			response.setContentType( "text/plain" );
-			response.setHeader( "Cache-Control", "no-cache" );
-			response.setCharacterEncoding( "UTF-8" );
+		} else {
+			response.setContentType("text/plain");
+			response.setHeader("Cache-Control", "no-cache");
+			response.setCharacterEncoding("UTF-8");
 			try {
 				PrintWriter writer = response.getWriter();
-				writer.print( false );
-			}
-			catch( IOException e ) {
+				writer.print(false);
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	public void setServletContext( ServletContext servletContext ) {
+	public void setServletContext(ServletContext servletContext) {
 		this.servletContext = servletContext;
 	}
 }
